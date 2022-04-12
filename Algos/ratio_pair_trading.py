587317fe2208a81339bf87e7e18fb7136ddaf2fa
Nikolas <nikolaskjeldsen@gmail.com>
2022-04-12 22:13:52 +0200

    wrapping part of code in while loops

--- Algos/ratio_pair_trading.py
+++ Algos/ratio_pair_trading.py
@@ -5,6 +5,21 @@ from binance.client import Client
 from binance import ThreadedWebsocketManager
 from time import sleep
 
+
+""" MAIN FUNCTIONS """
+
+
+def start_multiplex_socket():
+    try:
+        twm.start()
+        twm.start_multiplex_socket(callback=process_messages, streams=streams)
+        return True
+
+    except:
+        twm.stop()
+        return False
+
+
 def normalizer(ratio):
     """
     Take a ratio of prices and make it standard normal, using param map
@@ -13,22 +28,34 @@ def normalizer(ratio):
     return
 
 
-def process_messages(msg):
+def run_trade_logic():
+    try:
+        # Main trade logic here
+        return True
 
-     #If we disconnected and could not reconnect
+    except:
+        # TODO: logging
+        return False
+
+
+
+def process_messages(msg):
+    # If we disconnected and could not reconnect
     if msg['data']['e'] == 'error':
         twm.stop()
         sleep(2)
-        twm.start()
+        start_multiplex_socket()
 
     else:
         print(msg)
 
 
 def main():
+    global param_map, streamed_data, twm, streams, api_key, api_secret
+    global socket_status, run_algo
 
-    global param_map, streamed_data, twm, streams
     set_envrionment_vars()
+    run_algo = True
     api_key = os.environ.get('binance_api')
     api_secret = os.environ.get('binance_secret')
     tickers_to_trade = ['BTCUSDT', 'ETHUSDT']
@@ -40,23 +67,27 @@ def main():
     streamed_data = {f'{k}/{v}' for k, v in pair_map.items()}
     streams = ['btcusdt@kline_1h', 'ethusdt@kline_1h']
 
+    while run_algo is True:
+        try:
+            client = Client(api_key, api_secret)
+            strat = normalizedRatio(n=72, interval='1h', ticker_list=tickers_to_trade, pair_map=pair_map, client=client)
+            param_map = strat.fit()
+            twm = ThreadedWebsocketManager(api_key=api_key, api_secret=api_secret)
+            socket_status = start_multiplex_socket()
 
-    try:
-        client = Client(api_key, api_secret)
-        strat = normalizedRatio(n=72, interval='1h', ticker_list=tickers_to_trade, pair_map=pair_map, client=client)
-        param_map = strat.fit()  # TODO: Instead of returning a dict, maybe return a normalizer function, or define another method in the class to normalize
-        twm = ThreadedWebsocketManager(api_key=api_key, api_secret=api_secret)
-        twm.start()
-        twm.start_multiplex_socket(callback=process_messages, streams=streams)
+            while socket_status is True:  # and twm.is_alive() is True: ???
+                socket_status = run_trade_logic()
 
-        # This runs after
-        for i in range(10):
-            print(i)
+        except:
+            twm.stop()
+            print('total fatal error in except in main(), bro')
+            run_algo = False
+            # TODO: logging and notifying via emial
+            # TODO: Here we can set conditions (or have different except statements) to set run_algo to False and quit
+            # TODO: Before we quit, we might want to exit all open positions
 
-    except:
-        twm.stop()
-        print('total fatal error in except in main(), bro')
+        print('if this runs, socket_status is False, i.e. the socket died / closed')
+
 
 if __name__ == '__main__':
     main()
-
