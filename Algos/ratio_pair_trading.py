ce342750fa79add3e6b960ebb7b5fb4a7b7524bf
Nikolas <nikolaskjeldsen@gmail.com>
2022-04-12 21:06:54 +0200

    multiplex socket test

--- Algos/ratio_pair_trading.py
+++ Algos/ratio_pair_trading.py
@@ -3,6 +3,7 @@ from Strategies.pairs_trading import normalizedRatio
 from Modules.configure_api import set_envrionment_vars
 from binance.client import Client
 from binance import ThreadedWebsocketManager
+from time import sleep
 
 def normalizer(ratio):
     """
@@ -12,33 +13,45 @@ def normalizer(ratio):
     return
 
 
-def trader(msg):
-    """
-    Main callback function for when web
-    :return:
-    """
-    print(msg)
+def process_messages(msg):
+
+     #If we disconnected and could not reconnect
+    if msg['data']['e'] == 'error':
+        twm.stop()
+        sleep(2)
+        twm.start()
+
+    else:
+        print(msg)
 
 
 def main():
-    global param_map, streamed_data
+
+    global param_map, streamed_data, twm, streams
     set_envrionment_vars()
     api_key = os.environ.get('binance_api')
     api_secret = os.environ.get('binance_secret')
     tickers_to_trade = ['BTCUSDT', 'ETHUSDT']
+
     pair_map = {
         'BTCUSDT': 'ETHUSDT'
     }
+
     streamed_data = {f'{k}/{v}' for k, v in pair_map.items()}
+    streams = ['btcusdt@kline_1h', 'ethusdt@kline_1h']
 
 
     try:
         client = Client(api_key, api_secret)
         strat = normalizedRatio(n=72, interval='1h', ticker_list=tickers_to_trade, pair_map=pair_map, client=client)
         param_map = strat.fit()  # TODO: Instead of returning a dict, maybe return a normalizer function, or define another method in the class to normalize
+        twm = ThreadedWebsocketManager(api_key=api_key, api_secret=api_secret)
+        twm.start()
+        twm.start_multiplex_socket(callback=process_messages, streams=streams)
 
     except:
-        pass
+        twm.stop()
+        print('total fatal error in except in main(), bro')
 
 if __name__ == '__main__':
     main()
